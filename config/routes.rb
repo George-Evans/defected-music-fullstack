9b81e97152560c8bb0fcf968f671c3645c1736f6
George Evans <george.r.evans@googlemail.com>
2019-06-08 22:22:41 +0200

    set artists#index as root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :artists
   resources :homes
   resources :abouts
+
+  root 'artists#index'
   get '/about', to: 'simple_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
