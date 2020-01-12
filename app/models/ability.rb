542038b2feeeac1d6304c007b46366b490d58a79
George Evans <george.r.evans@googlemail.com>
2020-01-12 22:34:58 +0100

    add read permissions for Songwriter & Catalogue by default

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,8 @@ class Ability
 
   def initialize(user)
     can :read, Artist 
+    can :read, Songwriter
+    can :read, Catalogue
     can :read, Editorial  
     if user.present?  # additional permissions for logged in users (they can manage their posts)
         can :manage, :all
